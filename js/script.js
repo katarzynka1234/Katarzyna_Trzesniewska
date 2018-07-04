bf4eae3bd0282db1d3b10ee4a05496aa725aaf5a
Katarzyna Trześniewska <katarzynka.f@gmail.com>
2018-07-04 14:04:24 +0200

    Add js code - doesn't work properly!

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+// $(document).ready(function () {   
+   //====== KOD DZIAŁA TYLKO NA PIERWSZY PROJEKT ===========// 
+
+//         $('#more-details').click(function(){
+//             $('#more-details').parent().addClass("more-description")
+//     })
+    
+
+
+
+// });
