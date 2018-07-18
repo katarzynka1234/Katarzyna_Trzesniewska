45d11b07b15e0442b6d99aca36789ba8e172a7eb
Katarzyna Trześniewska <katarzynka.f@gmail.com>
2018-07-18 17:34:13 +0200

    Edit background & add animation

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
 
 
-$(document).ready(function () {   
-
+$(document).ready(function () {
+    
+    // == Read More button - toggle effect ===//
     var btnReadMore = $('#read_more');
-    var moreAboutMe = $('.not-visible'); 
-
+    var moreAboutMe = $('.not-visible');
 
     btnReadMore.click(function(){
         moreAboutMe.toggle('slow');
-    });     
+    }); 
 
+
+    // === TECHNOLOGIES SECTION - iconS animation ===//
+    
+    // *** SCROLL MAGIC plugin ***//
+    //Init ScrollMagic   
+    var animatedEffect = 'fadeInDownBig'; 
+    var controller = new ScrollMagic.Controller();    
+
+    //animatedIcon.addClass('not-show');
+    //build a scene
+    var sceneOne = new ScrollMagic.Scene({
+        triggerElement: '#technology'      
+    })         
+    .setClassToggle('#icon', animatedEffect)
+    .addTo(controller); 
+    
+    var sceneTwo = new ScrollMagic.Scene({
+        triggerElement: '#technology'
+    })
+    .setClassToggle('#icon2', animatedEffect)
+    .addTo(controller);
+
+    
 });
